3cc8311d79c03be5f0010c07506ba901bdccddf6
lyn <3228562873@qq.com>
2024-08-15 16:36:59 +0800

    项目初始化

---
+++ niandada-backend/src/main/java/com/lyn/niandada/constant/FileConstant.java
@@ -0,0 +1,15 @@
+package com.lyn.niandada.constant;
+
+/**
+ * 文件常量
+ *
+
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://lyn-1306837533.cos.ap-shanghai.myqcloud.com";
+}
---
+++ niandada-backend/src/main/java/com/lyn/niandada/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lyn.niandada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lyn.niandada.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
